e561bbead3a26bbbbd587369c75370c3b45e42a6
smartobject2 <info@sheltonresearch.com>
2013-08-15 09:40:30 -0500

    Correcting repository

---
+++ ModernUIApp2/Pages/NewPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+
+namespace ModernUIApp2.Pages
+{
+    /// <summary>
+    /// Interaction logic for NewPage.xaml
+    /// </summary>
+    public partial class NewPage : UserControl
+    {
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+
+        public NewPage()
+        {
+            InitializeComponent();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan();
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            this.screenControl1.UpdateScreen();
+        }
+
+
+
+    }
+}
